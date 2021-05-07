7057dc82a53e6b238f3f0bbc51c29cd282f337df
Orion Junkins <junkinso@oregonstate.edu>
2021-05-07 11:23:00 -0700

    Extract Class  'button' to address Data Clumps

--- main.rb
+++ main.rb
@@ -1,9 +1,25 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+def draw_button(button, foreground_color, is_dark_mode)
   if is_dark_mode
     # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    paint(button, foreground_color - 10, '#111111')
   else
     # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    paint(button, foreground_color + 10, '#E0E0E0')
   end
 end
+
+class button
+  attr_reader :label_text, :x, :y
+  def initialize(label_text, x, y)
+    @label_text = label_text 
+    @x = x 
+    @y = y
+  end
+end
+
+
+
+
+
+# Smell -> Refactoring
+  # Data Clumps -> Extract class
