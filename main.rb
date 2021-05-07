f6513658d1135ba775b0bd7ed9b037c83466b2a2
Orion Junkins <junkinso@oregonstate.edu>
2021-05-07 11:43:13 -0700

    draw_button signature reverted to align with original

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
-def draw_button(button, foreground_color, is_dark_mode)
+def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+  button = Button.new(label_text, x, y, foreground_color, is_dark_mode)
   print("Warning: depricated function, use button.draw")
-  button.draw(foreground_color, is_dark_mode)
+  button.draw()
 end
 
-class button
+class Button
   def initialize(label_text, x, y, foreground_color, is_dark_mode=false)
     @label_text = label_text 
     @x = x 
@@ -29,7 +30,7 @@ class button
   end
 
   end
-  
+
   def draw(foreground_color, is_dark_mode)
     paint(label_text, x, y, foreground_color, button_color)
   end
